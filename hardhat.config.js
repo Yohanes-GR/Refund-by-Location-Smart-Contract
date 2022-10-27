bb62889c18674f26d5f1a45d1ef4bbd4cc24253c
Yohanes-GR <yohgut@gmail.com>
2022-10-27 02:25:26 -0400

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks:{
+  goerli: {
+    url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+    accounts: [GOERLI_PRIVATE_KEY]
+  }}
+};
